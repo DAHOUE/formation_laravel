7dfb3af98494eaeb5489b603d9860b45ea5ef1d1
Isaac <isaacdahoue1@gmail.com>
2021-03-12 12:00:35 +0100

    add images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,7 @@
 @section('title', 'Hello |'.config('project.variable'))
 
 @section('content')
+<img src="/images/flutter_location.png" alt="Package flutter location">
 <h1> Page d'accueil<h1>
     {{ config('project.variable') }}
 
