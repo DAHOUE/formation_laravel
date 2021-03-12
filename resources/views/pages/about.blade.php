000bfa7dbb1df4ae3c2d67bb5113a19a6ea30247
Isaac <isaacdahoue1@gmail.com>
2021-03-12 12:19:07 +0100

    Make use of asset function help toload image

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('title', config('app.name'))
 
 @section('content')
-<img src="/images/parfums.jpg" alt="Les parfums">
+<img src="{{ asset('images/parfums.jpg') }}" alt="Les parfums">
 <h1> Page Propos de nous<h1>
     <h1> <a href="{{ route('app_home') }}">Allez à l'accueil<a><h1>
 @endsection
